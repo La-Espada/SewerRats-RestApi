e667f8aa4a6dd851a3cfb7de763a3edca07fbcb7
Cemil Aslan <cemil.onepiece@hotmail.com>
2023-04-30 00:22:11 +0200

    added puml

---
+++ SewerRats-App/src/main/java/com/sewerrats/sewerratsapp/SewerRatsAppApplication.java
@@ -0,0 +1,13 @@
+package com.sewerrats.sewerratsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SewerRatsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SewerRatsAppApplication.class, args);
+    }
+
+}
---
+++ SewerRats-App/src/test/java/com/sewerrats/sewerratsapp/SewerRatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sewerrats.sewerratsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SewerRatsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
